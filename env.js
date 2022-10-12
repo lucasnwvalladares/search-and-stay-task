483ff94ba5494714793ee39890f9ddfbe276d3dd
lucasnwvalladares <lucasnwvalladares@gmail.com>
2022-10-12 00:08:03 -0300

    first step - retrieving the bearer auth access token

---
+++ env.js
@@ -0,0 +1,6 @@
+export default {
+  apiLogin: {
+    email: process.env.API_LOGIN_EMAIL,
+    password: process.env.API_LOGIN_PASSWORD
+  }
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,28 @@
 <template>
-  
+  <div>Hello World</div>
 </template>
 
 <script>
+import ENV from '@/env'
+
 export default {
   name: 'IndexPage',
-  data() {
+  data () {
     return {
-      
+
     }
   },
+
+  created () {
+    const body = {
+      login: {
+        email: ENV.apiLogin.email,
+        password: ENV.apiLogin.password
+      }
+    }
+    this.$axios.$post('https://sys-dev.searchandstay.com/api/admin/login_json', body).then((response) => {
+      console.log(response)
+    })
+  }
 }
 </script>
